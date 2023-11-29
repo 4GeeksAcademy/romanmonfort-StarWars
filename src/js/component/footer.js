64b19e8d242b781b3834243392bdf364cd167aa8
romanmonfort <137888802+romanmonfort@users.noreply.github.com>
2023-11-29 02:03:54 +0000

    design

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+	<footer className="footer mt-auto py-3 text-center"   >
+		<div className="row " >
+		<h6>Todos los derechos reservados @CopyRight</h6>
+		</div>
 	</footer>
 );
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/home.css";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-dark mb-3 ">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+		<nav className="navbar navbar-light  mb-3 text-white border solid p-2">
+			<Link to="/" className="text-decoration-none">
+				<span className="navbar-brand mb-0 h1 text-white">HOME</span>
 			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
+			<Link to="/" className="text-decoration-none">
+				<span className="navbar-brand mb-0 h1 text-white">SPECIES </span>
+			</Link>
+			<h2 className="star">STAR WARS</h2>
+			<Link to="/" className="text-decoration-none">
+				<span className="navbar-brand mb-0 h1 text-white">STARSHIPS </span>
+			</Link>
+			<Link to="/" className="text-decoration-none">
+				<span className="navbar-brand mb-0 h1 text-white">VEHICLES </span>
+			</Link>
 		</nav>
 	);
 };
